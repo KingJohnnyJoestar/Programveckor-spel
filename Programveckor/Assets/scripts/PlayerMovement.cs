8166c8a39e4d57972c3458e66766012d10ef9e5a
hugocasserfelt <hugo.casserfelt@elev.ga.lbs.se>
2026-01-08 10:55:10 +0100

    a

--- Programveckor/Assets/scripts/PlayerMovement.cs
+++ Programveckor/Assets/scripts/PlayerMovement.cs
@@ -25,11 +25,11 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
         {
-            rb.AddForce(new Vector2(movementSpeed,0));
+            rb.AddForce(new Vector2(-movementSpeed,0));
         }
         else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
         {
-            rb.AddForce(new Vector2(-movementSpeed, 0));
+            rb.AddForce(new Vector2(movementSpeed, 0));
         }
         if ((Keyboard.current.zKey.wasPressedThisFrame || Keyboard.current.spaceKey.wasPressedThisFrame) && touchingGround()) // start off jump
         {
