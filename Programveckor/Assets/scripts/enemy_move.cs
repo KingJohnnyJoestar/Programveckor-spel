1b9bdf6f41fc23c219148904154f5d41f4188887
GAYFEMBOYTAKEN <ludvig.werner@elev.ga.lbs.se>
2026-01-09 11:58:11 +0100

    Better chase script UwU

--- Programveckor/Assets/scripts/enemy_move.cs
+++ Programveckor/Assets/scripts/enemy_move.cs
@@ -1,28 +1,61 @@
+using Unity.VisualScripting;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class enemy_move : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float speed = 1;
-    public float PlayerAlertDistance = 5; 
-    public GameObject Enemy;
-    public GameObject Player;
+    public GameObject Enemy; // Put on enemy 
+    public GameObject Player; // Put on the player
 
+    public float speed = 1; // Speed - Recomended 0.05
+    public float EnemyrAlertDistance = 5; 
+    public float EnemyChasseMaxDistance = 10;
+    
+    private float Distance;
+    private bool ActiveAtack; 
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
        
     {
-        // Chase script
-        float Distance = Vector2.Distance(Enemy.transform.position, Player.transform.position); // Measures player distance with the enemy
-
-
-        if (Distance < PlayerAlertDistance) //Trigers when close to the enemy
-        {
-            Enemy.transform.position = Vector2.MoveTowards(Enemy.transform.position, Player.transform.position, speed);
+        Distance = GetDistance(); 
+        ActiveAtack = GetAtackValidationCalculation();
+        if (ActiveAtack == true)
+            {
+            Chasse();
         }
+    }
+    private void Chasse() // Moves enemy towards player.
+    {
+        Enemy.transform.position = Vector2.MoveTowards(Enemy.transform.position, Player.transform.position, speed);
+        return;
+    }
+    private float GetDistance() // Mesures player & Enemy distance in X & Y
+    {
+        float Distance = Vector2.Distance(Enemy.transform.position, Player.transform.position);
+        return Distance;
+    }
+    private bool GetAtackValidationCalculation()
+    {
+        if (ActiveAtack == true)
+        {
+            if (Distance <= EnemyChasseMaxDistance)
+            {
+                return true;
+            }
+        }
+        if (ActiveAtack == false)
+        {
+            if (Distance <= EnemyrAlertDistance)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
