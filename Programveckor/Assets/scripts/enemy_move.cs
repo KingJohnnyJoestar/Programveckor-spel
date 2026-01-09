9ecf484c2aa3aea44e75b1f42ac9ed5fe57f6bf4
GAYFEMBOYTAKEN <ludvig.werner@elev.ga.lbs.se>
2026-01-09 23:05:33 +0100

    MRAOW
    meow maow mraooow meooooooooooow maow

--- Programveckor/Assets/scripts/enemy_move.cs
+++ Programveckor/Assets/scripts/enemy_move.cs
@@ -8,31 +8,45 @@ public class enemy_move : MonoBehaviour
     public GameObject Enemy; // Put on enemy 
     public GameObject Player; // Put on the player
 
-    public float speed = 1; // Speed - Recomended 0.05
-    public float EnemyrAlertDistance = 5; 
-    public float EnemyChasseMaxDistance = 10;
+    // Enemy speed & chasse distance (Non changebal)
+    public float speed = 1; //                   Speed - Recomended 0.05
+    public float EnemyrAlertDistance = 5; //     Enemy minmum distance to player to start a chase
+    public float EnemyChasseMaxDistance = 10; // Enemy minmum distances to discontinue an active chase
     
+    // Enemy data (Can't be changed)
+    private Vector2 EnemySpawnPosition;
     private float Distance;
     private bool ActiveAtack; 
+
     void Start()
     {
-
+        EnemySpawnPosition = Enemy.transform.position;
     }
 
     // Update is called once per frame
     void Update()
-       
+    
     {
         Distance = GetDistance(); 
         ActiveAtack = GetAtackValidationCalculation();
+
         if (ActiveAtack == true)
-            {
+        {
             Chasse();
+        }
+        else
+        {
+            ReturninToSpawn();
         }
     }
     private void Chasse() // Moves enemy towards player.
     {
         Enemy.transform.position = Vector2.MoveTowards(Enemy.transform.position, Player.transform.position, speed);
+        return;
+    }
+    private void ReturninToSpawn()
+    {
+        Enemy.transform.position = Vector2.MoveTowards(Enemy.transform.position, EnemySpawnPosition, speed);
         return;
     }
     private float GetDistance() // Mesures player & Enemy distance in X & Y
@@ -49,6 +63,7 @@ public class enemy_move : MonoBehaviour
                 return true;
             }
         }
+        
         if (ActiveAtack == false)
         {
             if (Distance <= EnemyrAlertDistance)
