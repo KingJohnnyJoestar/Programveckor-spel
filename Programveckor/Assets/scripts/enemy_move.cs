d38c48b8d94f014e51a98d5cf5bc30fb8962acf8
GAYFEMBOYTAKEN <ludvig.werner@elev.ga.lbs.se>
2026-01-09 10:40:47 +0100

    Update enemy_move.cs

--- Programveckor/Assets/scripts/enemy_move.cs
+++ Programveckor/Assets/scripts/enemy_move.cs
@@ -3,21 +3,26 @@ using UnityEngine;
 public class enemy_move : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float speed;
+    public float speed = 1;
+    public float PlayerAlertDistance = 5;
     public GameObject Enemy;
     public GameObject Player;
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
+       
     {
-        Enemy.transform.position = Vector2.MoveTowards(Enemy.transform.position, Player.transform.position, speed);
-    }
-    private void OnCollisionEnter(Collision Player)
-    {
-        
+        // Chase script
+        float Distance = Vector2.Distance(Enemy.transform.position, Player.transform.position); // Measures player distance with the enemy
+
+
+        if (Distance < PlayerAlertDistance) //Trigers when close to the enemy
+        {
+            Enemy.transform.position = Vector2.MoveTowards(Enemy.transform.position, Player.transform.position, speed);
+        }
     }
 }
