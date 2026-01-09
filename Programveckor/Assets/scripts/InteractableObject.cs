49fc1a71f6583f700f08e024f0ffa278c532105e
hugocasserfelt <hugo.casserfelt@elev.ga.lbs.se>
2026-01-09 11:03:12 +0100

    ny grej

---
+++ Programveckor/Assets/scripts/InteractableObject.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractableObject : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject interactUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactUI.activeSelf)
+        {
+            if(Keyboard.current.eKey.wasPressedThisFrame || Keyboard.current.xKey.wasPressedThisFrame)
+            {
+                Debug.Log("interact");
+                Interact();
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            interactUI.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            interactUI.SetActive(false);
+        }
+    }
+    public virtual void Interact()
+    {
+
+    }
+}
